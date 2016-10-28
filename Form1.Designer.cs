586582df3065ee499a51bf2fcda017948c9317cb
Willster419 <willster419@outlook.com>
2016-10-28 01:10:37 -0400

    making a Backup of the microsoft SQL server one in case we need to go back to it

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,285 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.Label phoneNameLabel;
+            this.phoneDatabaseDataSet = new GameOfPhones.PhoneDatabaseDataSet();
+            this.phoneBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.phoneTableAdapter = new GameOfPhones.PhoneDatabaseDataSetTableAdapters.PhoneTableAdapter();
+            this.tableAdapterManager = new GameOfPhones.PhoneDatabaseDataSetTableAdapters.TableAdapterManager();
+            this.phoneBindingNavigator = new System.Windows.Forms.BindingNavigator(this.components);
+            this.bindingNavigatorMoveFirstItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMovePreviousItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorPositionItem = new System.Windows.Forms.ToolStripTextBox();
+            this.bindingNavigatorCountItem = new System.Windows.Forms.ToolStripLabel();
+            this.bindingNavigatorSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorMoveNextItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorMoveLastItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.bindingNavigatorAddNewItem = new System.Windows.Forms.ToolStripButton();
+            this.bindingNavigatorDeleteItem = new System.Windows.Forms.ToolStripButton();
+            this.phoneBindingNavigatorSaveItem = new System.Windows.Forms.ToolStripButton();
+            this.phoneDataGridView = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.phoneNameComboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            phoneNameLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneDatabaseDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneBindingNavigator)).BeginInit();
+            this.phoneBindingNavigator.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // phoneDatabaseDataSet
+            // 
+            this.phoneDatabaseDataSet.DataSetName = "PhoneDatabaseDataSet";
+            this.phoneDatabaseDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // phoneBindingSource
+            // 
+            this.phoneBindingSource.DataMember = "Phone";
+            this.phoneBindingSource.DataSource = this.phoneDatabaseDataSet;
+            // 
+            // phoneTableAdapter
+            // 
+            this.phoneTableAdapter.ClearBeforeFill = true;
+            // 
+            // tableAdapterManager
+            // 
+            this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.PhoneTableAdapter = this.phoneTableAdapter;
+            this.tableAdapterManager.UpdateOrder = GameOfPhones.PhoneDatabaseDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
+            // 
+            // phoneBindingNavigator
+            // 
+            this.phoneBindingNavigator.AddNewItem = this.bindingNavigatorAddNewItem;
+            this.phoneBindingNavigator.BindingSource = this.phoneBindingSource;
+            this.phoneBindingNavigator.CountItem = this.bindingNavigatorCountItem;
+            this.phoneBindingNavigator.DeleteItem = this.bindingNavigatorDeleteItem;
+            this.phoneBindingNavigator.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bindingNavigatorMoveFirstItem,
+            this.bindingNavigatorMovePreviousItem,
+            this.bindingNavigatorSeparator,
+            this.bindingNavigatorPositionItem,
+            this.bindingNavigatorCountItem,
+            this.bindingNavigatorSeparator1,
+            this.bindingNavigatorMoveNextItem,
+            this.bindingNavigatorMoveLastItem,
+            this.bindingNavigatorSeparator2,
+            this.bindingNavigatorAddNewItem,
+            this.bindingNavigatorDeleteItem,
+            this.phoneBindingNavigatorSaveItem});
+            this.phoneBindingNavigator.Location = new System.Drawing.Point(0, 0);
+            this.phoneBindingNavigator.MoveFirstItem = this.bindingNavigatorMoveFirstItem;
+            this.phoneBindingNavigator.MoveLastItem = this.bindingNavigatorMoveLastItem;
+            this.phoneBindingNavigator.MoveNextItem = this.bindingNavigatorMoveNextItem;
+            this.phoneBindingNavigator.MovePreviousItem = this.bindingNavigatorMovePreviousItem;
+            this.phoneBindingNavigator.Name = "phoneBindingNavigator";
+            this.phoneBindingNavigator.PositionItem = this.bindingNavigatorPositionItem;
+            this.phoneBindingNavigator.Size = new System.Drawing.Size(626, 25);
+            this.phoneBindingNavigator.TabIndex = 0;
+            this.phoneBindingNavigator.Text = "bindingNavigator1";
+            // 
+            // bindingNavigatorMoveFirstItem
+            // 
+            this.bindingNavigatorMoveFirstItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveFirstItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveFirstItem.Image")));
+            this.bindingNavigatorMoveFirstItem.Name = "bindingNavigatorMoveFirstItem";
+            this.bindingNavigatorMoveFirstItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveFirstItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMoveFirstItem.Text = "Move first";
+            // 
+            // bindingNavigatorMovePreviousItem
+            // 
+            this.bindingNavigatorMovePreviousItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMovePreviousItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMovePreviousItem.Image")));
+            this.bindingNavigatorMovePreviousItem.Name = "bindingNavigatorMovePreviousItem";
+            this.bindingNavigatorMovePreviousItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMovePreviousItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorMovePreviousItem.Text = "Move previous";
+            // 
+            // bindingNavigatorSeparator
+            // 
+            this.bindingNavigatorSeparator.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator.Size = new System.Drawing.Size(6, 25);
+            // 
+            // bindingNavigatorPositionItem
+            // 
+            this.bindingNavigatorPositionItem.AccessibleName = "Position";
+            this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
+            this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
+            this.bindingNavigatorPositionItem.Text = "0";
+            this.bindingNavigatorPositionItem.ToolTipText = "Current position";
+            // 
+            // bindingNavigatorCountItem
+            // 
+            this.bindingNavigatorCountItem.Name = "bindingNavigatorCountItem";
+            this.bindingNavigatorCountItem.Size = new System.Drawing.Size(35, 15);
+            this.bindingNavigatorCountItem.Text = "of {0}";
+            this.bindingNavigatorCountItem.ToolTipText = "Total number of items";
+            // 
+            // bindingNavigatorSeparator1
+            // 
+            this.bindingNavigatorSeparator1.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator1.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorMoveNextItem
+            // 
+            this.bindingNavigatorMoveNextItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveNextItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveNextItem.Image")));
+            this.bindingNavigatorMoveNextItem.Name = "bindingNavigatorMoveNextItem";
+            this.bindingNavigatorMoveNextItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveNextItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveNextItem.Text = "Move next";
+            // 
+            // bindingNavigatorMoveLastItem
+            // 
+            this.bindingNavigatorMoveLastItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorMoveLastItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorMoveLastItem.Image")));
+            this.bindingNavigatorMoveLastItem.Name = "bindingNavigatorMoveLastItem";
+            this.bindingNavigatorMoveLastItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorMoveLastItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorMoveLastItem.Text = "Move last";
+            // 
+            // bindingNavigatorSeparator2
+            // 
+            this.bindingNavigatorSeparator2.Name = "bindingNavigatorSeparator";
+            this.bindingNavigatorSeparator2.Size = new System.Drawing.Size(6, 6);
+            // 
+            // bindingNavigatorAddNewItem
+            // 
+            this.bindingNavigatorAddNewItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorAddNewItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorAddNewItem.Image")));
+            this.bindingNavigatorAddNewItem.Name = "bindingNavigatorAddNewItem";
+            this.bindingNavigatorAddNewItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorAddNewItem.Size = new System.Drawing.Size(23, 22);
+            this.bindingNavigatorAddNewItem.Text = "Add new";
+            // 
+            // bindingNavigatorDeleteItem
+            // 
+            this.bindingNavigatorDeleteItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.bindingNavigatorDeleteItem.Image = ((System.Drawing.Image)(resources.GetObject("bindingNavigatorDeleteItem.Image")));
+            this.bindingNavigatorDeleteItem.Name = "bindingNavigatorDeleteItem";
+            this.bindingNavigatorDeleteItem.RightToLeftAutoMirrorImage = true;
+            this.bindingNavigatorDeleteItem.Size = new System.Drawing.Size(23, 20);
+            this.bindingNavigatorDeleteItem.Text = "Delete";
+            // 
+            // phoneBindingNavigatorSaveItem
+            // 
+            this.phoneBindingNavigatorSaveItem.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.phoneBindingNavigatorSaveItem.Image = ((System.Drawing.Image)(resources.GetObject("phoneBindingNavigatorSaveItem.Image")));
+            this.phoneBindingNavigatorSaveItem.Name = "phoneBindingNavigatorSaveItem";
+            this.phoneBindingNavigatorSaveItem.Size = new System.Drawing.Size(23, 23);
+            this.phoneBindingNavigatorSaveItem.Text = "Save Data";
+            this.phoneBindingNavigatorSaveItem.Click += new System.EventHandler(this.phoneBindingNavigatorSaveItem_Click_2);
+            // 
+            // phoneDataGridView
+            // 
+            this.phoneDataGridView.AutoGenerateColumns = false;
+            this.phoneDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.phoneDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2});
+            this.phoneDataGridView.DataSource = this.phoneBindingSource;
+            this.phoneDataGridView.Location = new System.Drawing.Point(12, 28);
+            this.phoneDataGridView.Name = "phoneDataGridView";
+            this.phoneDataGridView.Size = new System.Drawing.Size(300, 220);
+            this.phoneDataGridView.TabIndex = 1;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Id";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Id";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "PhoneName";
+            this.dataGridViewTextBoxColumn2.HeaderText = "PhoneName";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // phoneNameLabel
+            // 
+            phoneNameLabel.AutoSize = true;
+            phoneNameLabel.Location = new System.Drawing.Point(318, 28);
+            phoneNameLabel.Name = "phoneNameLabel";
+            phoneNameLabel.Size = new System.Drawing.Size(72, 13);
+            phoneNameLabel.TabIndex = 2;
+            phoneNameLabel.Text = "Phone Name:";
+            // 
+            // phoneNameComboBox
+            // 
+            this.phoneNameComboBox.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.phoneBindingSource, "PhoneName", true));
+            this.phoneNameComboBox.FormattingEnabled = true;
+            this.phoneNameComboBox.Location = new System.Drawing.Point(396, 25);
+            this.phoneNameComboBox.Name = "phoneNameComboBox";
+            this.phoneNameComboBox.Size = new System.Drawing.Size(121, 21);
+            this.phoneNameComboBox.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 105);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 350);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(phoneNameLabel);
+            this.Controls.Add(this.phoneNameComboBox);
+            this.Controls.Add(this.phoneDataGridView);
+            this.Controls.Add(this.phoneBindingNavigator);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.phoneDatabaseDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneBindingNavigator)).EndInit();
+            this.phoneBindingNavigator.ResumeLayout(false);
+            this.phoneBindingNavigator.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.phoneDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private PhoneDatabaseDataSet phoneDatabaseDataSet;
+        private System.Windows.Forms.BindingSource phoneBindingSource;
+        private PhoneDatabaseDataSetTableAdapters.PhoneTableAdapter phoneTableAdapter;
+        private PhoneDatabaseDataSetTableAdapters.TableAdapterManager tableAdapterManager;
+        private System.Windows.Forms.BindingNavigator phoneBindingNavigator;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorAddNewItem;
+        private System.Windows.Forms.ToolStripLabel bindingNavigatorCountItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorDeleteItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveFirstItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMovePreviousItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator;
+        private System.Windows.Forms.ToolStripTextBox bindingNavigatorPositionItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator1;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveNextItem;
+        private System.Windows.Forms.ToolStripButton bindingNavigatorMoveLastItem;
+        private System.Windows.Forms.ToolStripSeparator bindingNavigatorSeparator2;
+        private System.Windows.Forms.ToolStripButton phoneBindingNavigatorSaveItem;
+        private System.Windows.Forms.DataGridView phoneDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.ComboBox phoneNameComboBox;
+        private System.Windows.Forms.Button button1;
+
+
     }
 }
 
---
+++ Form2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace GameOfPhones
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            string connectString = "Server=10.14.52.54;Database=chinook;Uid=root;Pwd=;";
+            MySqlConnection conn = new MySqlConnection(connectString);
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT * FROM Track WHERE Track.TrackId=69";
+            MySqlDataAdapter adap = new MySqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            adap.Fill(ds);
+            dataGridView1.DataSource = ds.Tables[0].DefaultView;
+        }
+    }
+}
